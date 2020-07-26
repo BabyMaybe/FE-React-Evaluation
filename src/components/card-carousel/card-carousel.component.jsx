4691874ccdc2230111c345a0e6f24308884705c1
Eric Varela <eric_varela@mac.com>
2020-07-26 18:24:03 -0400

    Stubbed in Card Carousel Styling

---
+++ src/components/card-carousel/card-carousel.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './card-carousel.styles.scss';
+import Card from '../card/card.component';
+
+const CardCarousel = () => (
+  <div className="card-carousel">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+);
+
+export default CardCarousel;
