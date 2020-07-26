e128bbe6e4ec3173b26967454330663a77ce7f0f
Eric Varela <eric_varela@mac.com>
2020-07-26 18:24:58 -0400

    Stubbed In Home Page Styling

---
+++ src/pages/Home/home.page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './home.styles.scss';
+import HeaderBar from '../../components/header-bar/header-bar.component';
+import Card from '../../components/card/card.component';
+import CardCarousel from '../../components/card-carousel/card-carousel.component';
+
+const HomePage = () => (
+  <div className="home-page">
+    <HeaderBar />
+    <div className="page-container">
+      <h1 className="welcome-header">Welcome [USERNAME]</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus pronin sapien nunc accuan eget.</p>
+      <CardCarousel />
+    </div>
+  </div>
+);
+
+export default HomePage;
