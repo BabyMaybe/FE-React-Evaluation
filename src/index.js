b5d8dd7595d20793ad01d0966347104675141518
Eric Varela <eric_varela@mac.com>
2020-07-28 00:59:51 -0400

    Refactored Store into own file

--- src/index.js
+++ src/index.js
@@ -2,21 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import reducer from './reducers';
+
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  reducer,
-  composeEnhancers(
-    applyMiddleware(thunk),
-  ),
-);
+import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
