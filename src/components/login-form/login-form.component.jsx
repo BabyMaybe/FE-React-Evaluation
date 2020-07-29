a6b1670d5284939d00f3a1e94c8a9a6425af64e4
Eric Varela <eric_varela@mac.com>
2020-07-29 02:05:16 -0400

    Added Login Handling

--- src/components/login-form/login-form.component.jsx
+++ src/components/login-form/login-form.component.jsx
@@ -1,24 +1,66 @@
 import React, { useState } from 'react';
+
+import { fakeAuthenticate } from '../../utilities/utilities';
+import { userLoggedIn } from '../../redux/authentication.slice';
+
 import NiInput from '../ni-input/ni-input.component';
 import NiButton from '../ni-button/ni-button.component';
+
 import './login-form.styles.scss';
+
+const LoginForm = () => {
+
   // Setup state using useState hook
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [usernameValidationMsg, setUsernameValidationMsg] = useState('');
   const [passwordValidationMsg, setPasswordValidationMsg] = useState('');
+
   // Keep state in sync with user input
   const onUsernameChanged = e => setUsername(e.target.value);
   const onPasswordChanged = e => setPassword(e.target.value);
 
-const LoginForm = () => (
-  <div className="login-form">
+  // Simulate a fake login attempt
+  const onLoginAttempt = e => {
+    e.preventDefault();
+    // Clear any previous error messages
+    setUsernameValidationMsg('');
+    setPasswordValidationMsg('');
+
+    // simulate hashing password so we don't send plaintext through request
+    const hashedPassword = `hashed${password}`;
+
+    // build a fake simplified request object
+    const request = {
+      username,
+      password: hashedPassword,
+    };
+
+    fakeAuthenticate(request).then(user => {
+      console.log('logged in successfully');
+      // store user in store
+      // redirect to home page
+    })
+      .catch(error => {
+        if (error.name === 'UsernameException') {
+          setUsernameValidationMsg(error.message);
+        }
+        if (error.name === 'PasswordException') {
+          setPasswordValidationMsg(error.message);
+        }
+      });
+  };
+
+  const canSubmit = username && password;
+
+  return (
     <form className="login-form">
-    <h2 className="form-header">Login to Our Magic Portal</h2>
+      <h2 className="form-header">Login to Our Magic Portal</h2>
       <NiInput type="text" placeholder="Username" name="username" id="username" validationMsg={usernameValidationMsg} handleChange={onUsernameChanged} autoComplete="username" />
       <NiInput type="password" placeholder="Password" name="password" id="password" validationMsg={passwordValidationMsg} handleChange={onPasswordChanged} autoComplete="password" />
       <NiButton onClick={onLoginAttempt} disabled={!canSubmit}>LOGIN</NiButton>
     </form>
-);
+  );
+};
 
 export default LoginForm;
