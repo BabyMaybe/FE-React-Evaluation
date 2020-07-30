7c84ec6252aa82cf53c7aae1dfc038be9c918bc6
Eric Varela <eric_varela@mac.com>
2020-07-29 23:58:51 -0400

    Allow for reloading and maintaining user state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import {
   Route,
@@ -12,17 +12,27 @@ import LoginPage from './pages/Login/login.page';
 
 import './App.css';
 import Authenticated from './pages/Authenticated/authenticated.page';
+import { userLoggedIn } from './redux/authentication.slice';
 
 function App() {
   const userAuthenticated = useSelector(state => state.authentication.currentUser);
   const userPreviouslyAuthenticated = sessionStorage.getItem('currentUser');
+  const dispatch = useDispatch();
+  const isAuthenticated = userAuthenticated || userPreviouslyAuthenticated;
+
+  // There is probably a better way to handle this but this is to accomodate
+  // reloading and reading from session where the state gets cleared
+  // to allow for users to maintain authentication within a session
+  if (userPreviouslyAuthenticated && !userAuthenticated) {
+    dispatch(userLoggedIn(userPreviouslyAuthenticated));
+  }
 
   return (
     <div className="App">
       <Switch>
         <Route exact path="/login" component={LoginPage} />
         <Route path="/">
-          {(userAuthenticated || userPreviouslyAuthenticated) ? <Authenticated /> : <Redirect to="/login" />}
+          {isAuthenticated ? <Authenticated /> : <Redirect to="/login" />}
         </Route>
       </Switch>
     </div>
