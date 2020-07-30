5b38e09af717c1af0fae2620a4fb93b8a262ed09
Eric Varela <eric_varela@mac.com>
2020-07-29 20:55:09 -0400

    Generate Nav Items From Array

--- src/components/header-bar/header-bar.component.jsx
+++ src/components/header-bar/header-bar.component.jsx
@@ -4,22 +4,24 @@ import { Link } from 'react-router-dom';
 import Logo from '../logo/logo.component';
 import { ReactComponent as UserIcon } from '../../assets/user-solid.svg';
 
-const HeaderBar = () => (
+const HeaderBar = () => {
+  const menuItems = ['home', 'interests', 'skills'];
+
+  return (
     <header className="header-bar">
+
     <Logo width="125px" />
+
     <nav className="nav-bar">
-
-      <span className="nav-item selected">
-        <Link to="/">Home</Link>
+        {
+          menuItems.map(item => (
+            <span className="nav-item" key={item}>
+              <Link to={`/${item}`}>
+                {item}
+              </Link>
       </span>
-
-      <span className="nav-item">
-        <Link to="/interests">Interests</Link>
-      </span>
-
-      <span className="nav-item">
-        <Link to="/skills">Skills</Link>
-      </span>
+          ))
+        }
 
       <div className="nav-item username">
         <UserIcon className="user-icon" />
