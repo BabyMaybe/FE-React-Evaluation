99c8a1719efa0ca3e49daada1b8a6dd06effe5da
Eric Varela <eric_varela@mac.com>
2020-07-31 18:49:30 -0400

    ES Lint Fixes

--- src/components/logo/logo.component.jsx
+++ src/components/logo/logo.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import  logoPath from '../../assets/no_inc_logo.png';
+import logoPath from '../../assets/no_inc_logo.png';
 
 import './logo.styles.scss';
 
-const Logo = ({width}) => (
+const Logo = ({ width }) => (
   <div className="logo-container">
-    <img src={logoPath} alt="" width={width}/>
+    <img src={logoPath} alt="" width={width} />
   </div>
-)
+);
 
 export default Logo;
