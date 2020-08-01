9a79091062b54767cdf72a08f91a174ec82f8d38
Eric Varela <eric_varela@mac.com>
2020-07-31 23:10:41 -0400

    Removed Unused Code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import usersReducer from './users.slice';
 import interestsReducer from './interests.slice';
 import skillsReducer from './skills.slice';
 import authenticationReducer from './authentication.slice';
 
 const store = configureStore({
   reducer: {
-    users: usersReducer,
     interests: interestsReducer,
     skills: skillsReducer,
     authentication: authenticationReducer,
