0f4d9f5aac62269fb217e71433faa0413b4f21fc
Eric Varela <eric@noinc.com>
2020-09-13 22:54:42 -0400

    Added "Add Card"

--- src/components/card-carousel/card-carousel.component.jsx
+++ src/components/card-carousel/card-carousel.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './card-carousel.styles.scss';
 import Card from '../card/card.component';
+import AddCard from '../card/add-card.component';
 
 const CardCarousel = ({ dataset, title }) => {
   const displayItems = dataset || [];
@@ -11,7 +12,8 @@ const CardCarousel = ({ dataset, title }) => {
         <h1 className="card-carousel-title">
           {title}
         </h1>
-        {displayItems.map(item => <Card data={item} urlStub={title} />)}
+        {displayItems.map(item => <Card data={item} urlStub={title} key={item._id} />)}
+        <AddCard urlStub={title} />
       </div>
     </div>
   );
